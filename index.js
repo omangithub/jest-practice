5bd26c8062eb8f1f07da7f9419badcd09743ec54
oman <smithowain@hotmail.com>
2025-05-20 22:35:20 +0900

    feat : Completed calculator, caesar cipher functions for project

--- index.js
+++ index.js
@@ -1,5 +1,77 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
 
+export function capitalize(str) {
+   let capital = str.split("");
+   capital[0] = capital[0].toUpperCase();
+   let newStr = capital.join("");
+   return newStr
+}
+
+export function reverseString(str) {
+    let stringArr = str.split("");
+    let emptyArr = []
+    for(let i=0;i<stringArr.length;i++) {
+    emptyArr.unshift(stringArr[i])
+    }
+    let stringRev = emptyArr.join("")
+    return stringRev
+}
+
+export const calculator = (()=> {
+
+  const add = (a, b)=> {
+  return a+b;
+  }
+
+  const subtract = (a,b)=> {
+    return a-b;
+  }
+
+  const multiply = (a,b) => {
+    return a*b;
+  }
+
+  const divide = (a,b)=> {
+    return a/b;
+  }
+
+  return {add, subtract, multiply, divide}
+})();
+
+export function caesarCipher (string, shiftFactor) {
+
+   let alphabet ="abcdefghijklmnopqrstuvwxyz".split("");
+   let alphabetUpper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+   let stringSplit = string.split("");
+   let alphabetIndex = []
+
+   for(let i=0;i<string.length;i++) {
+    let cipheredLetter;
+
+    if (alphabet.includes(stringSplit[i])) {
+      let letterAsNumber = parseInt(alphabet.indexOf(stringSplit[i]))+shiftFactor;
+      if (letterAsNumber>26) {
+        letterAsNumber=letterAsNumber-26
+      }
+    cipheredLetter = alphabet[letterAsNumber]
+    } else if (alphabetUpper.includes(stringSplit[i])) {
+      let letterAsNumber = parseInt(alphabetUpper.indexOf(stringSplit[i]))+shiftFactor;
+      if (letterAsNumber>26) {
+        letterAsNumber=letterAsNumber-26
+      }
+    cipheredLetter = alphabetUpper[letterAsNumber]
+    } else {
+      cipheredLetter = stringSplit[i]
+    }
+
+    alphabetIndex.push(cipheredLetter)
+   }
+
+   alphabetIndex=alphabetIndex.join("");
+
+   return alphabetIndex
+}
+
+console.log(caesarCipher("aVHc", 15))
--- index.test.js
+++ index.test.js
@@ -1,5 +1,21 @@
-const sum = require('./sum');
+import { capitalize, reverseString, calculator, caesarCipher } from "./index.js";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('takes a string and returns first letter capitalized', () => {
+  expect(capitalize("my string")).toBe("My string");
 });
+
+test('takes a string and reverses it', () => {
+  expect(reverseString("Hello")).toBe("olleH");
+});
+
+test('a calculator with add, subtract, divide and multiply', () => {
+  expect(calculator.add(1,3)).toBe(4);
+  expect(calculator.subtract(2,1)).toBe(1);
+  expect(calculator.multiply(4,2)).toBe(8);
+  expect(calculator.divide(10,2)).toBe(5)
+});
+
+test('convert a string using a Caeser cipher', ()=> {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+})
